217a83528d77b24d9265ba64b9ad3d6b7e0f3122
Robert Lankford <robbylankford@gmail.com>
2023-03-05 15:47:41 -0500

    solution to day 20 part 1

---
+++ advent-of-code/2022/day-20.py
@@ -0,0 +1,59 @@
+# Import Data
+with open("day-20.txt", 'r') as file:
+    encryption = list(map(int, file.readlines()))
+
+
+# Classes and Functions
+class Decrypt:
+    def __init__(self, encryption) -> None:
+        self.encryption = list(enumerate(encryption))
+        self.grove_coords = [0, 0, 0]
+    
+    def mix(self):
+        """Mix the encryption by moving the individual numbers by their values
+        
+        Args: none (self)
+        
+        Returns: updates self.encryption and self.grove_coords
+        """
+        length = len(self.encryption)
+        
+        for i in range(length):
+            while True:
+                if self.encryption[0][0] == i:
+                    break
+                self.encryption.append(self.encryption.pop(0))
+            
+            current = self.encryption.pop(0)
+            move_by = current[1] % (length - 1)
+            
+            for _ in range(move_by):
+                self.encryption.append(self.encryption.pop(0))
+            self.encryption.append(current)
+        
+        while self.encryption[0][1] != 0:
+            self.encryption.append(self.encryption.pop(0))
+        
+        for i in range(3):
+            coord_idx = (i + 1) * 1000
+            coordinate = self.encryption[coord_idx % len(self.encryption)]
+            self.grove_coords[i] = coordinate[1]
+    
+    def solve_part1(self):
+        """Solve Part 1 by finding the grove coordinates
+        
+        Args: none (self)
+
+        Returns:
+            int: self.grove_coords
+        """
+        self.mix()
+        
+        return self.grove_coords
+
+
+# Question 1
+decrypt = Decrypt(encryption)
+grove_coords = decrypt.solve_part1()
+
+print(f"Answer 1: {sum(grove_coords)}")
