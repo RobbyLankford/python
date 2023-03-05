e1fc32a77fe86763c69bb8af5ab20f91385c2464
Robert Lankford <robbylankford@gmail.com>
2023-03-05 16:08:21 -0500

    solution to day 20 part 2

--- advent-of-code/2022/day-20.py
+++ advent-of-code/2022/day-20.py
@@ -5,9 +5,10 @@ with open("day-20.txt", 'r') as file:
 
 # Classes and Functions
 class Decrypt:
-    def __init__(self, encryption) -> None:
-        self.encryption = list(enumerate(encryption))
+    def __init__(self, encryption, key = 1) -> None:
+        self.encryption = list((i, n * key) for i, n in enumerate(encryption))
         self.grove_coords = [0, 0, 0]
+    
     
     def mix(self):
         """Mix the encryption by moving the individual numbers by their values
@@ -39,21 +40,31 @@ class Decrypt:
             coordinate = self.encryption[coord_idx % len(self.encryption)]
             self.grove_coords[i] = coordinate[1]
     
-    def solve_part1(self):
-        """Solve Part 1 by finding the grove coordinates
+    
+    def solve(self, times=1):
+        """Solve Part 1 and Part 2 by finding the grove coordinates
         
-        Args: none (self)
+        Args:
+            times (int): the number of times to mix
 
         Returns:
             int: self.grove_coords
         """
-        self.mix()
+        for _ in range(times):
+            self.mix()
         
         return self.grove_coords
 
 
 # Question 1
 decrypt = Decrypt(encryption)
-grove_coords = decrypt.solve_part1()
+grove_coords = decrypt.solve()
 
 print(f"Answer 1: {sum(grove_coords)}")
+
+
+# Question 2
+decrypt = Decrypt(encryption, key=811589153)
+grove_coords = decrypt.solve(times=10)
+
+print(f"Answer 2: {sum(grove_coords)}")
